343666f0b7940d5b8c92e8794a9f48b89311a30e
Himansu Desai <himansudesai@gmail.com>
2021-01-09 19:54:57 -0800

    Include zoomg library and create a zoomg container based on that. Nice to see zoomg coming to life inside a React app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+import Atom from './shapes/atom.js';
+import Engine from './shapes/engine.js';
+
 
 function App() {
+
+  useEffect(() => {
+    const Zoomg = window['Zoomg'];
+    console.log(`Zoomg = ${Zoomg}`);
+    const svgEle = Zoomg.svgElement;
+    const Config = Zoomg.Config;
+    const ViewStack = Zoomg.ViewStack;
+    const Rectangle = Zoomg.Rectangle;
+    
+    const zoomgContainer = document.getElementById("zoomg-container");
+
+    Zoomg.new('zoomg-container').then( () => {
+      const context = new Rectangle("top-context", 0, 0, null, 0, 0);
+    
+      const ROW_COUNT = 25;
+      const COLUMN_COUNT = 120;
+    
+      const ATOM_SIZE = 8;
+      const ENGINE_SIZE = 8;
+      const anAtom = new Atom("dummy", 0, 0, null);
+      const anEngine = new Engine("dummy", 0, 0, null);
+      const ATOM_SCALE = Config.registerInitialSizeForShape(anAtom.getTypeName(), ATOM_SIZE);
+      const ENGINE_SCALE = Config.registerInitialSizeForShape(anEngine.getTypeName(), ENGINE_SIZE);
+    
+      let count = 0;
+      for (let i = 0; i < COLUMN_COUNT; i++) {
+        for (let j = 0; j < ROW_COUNT; j++) {
+          let atom = new Atom(`${++count}`, (ATOM_SIZE * i), (ATOM_SIZE * j), svgEle.group, ATOM_SCALE);
+          context.insert(atom);
+        }
+      }
+    
+      for (let i = 0; i < COLUMN_COUNT; i++) {
+        for (let j = 25; j < ROW_COUNT * 2; j++) {
+          let engine = new Engine(`${++count}`, (ENGINE_SIZE * i), (ENGINE_SIZE * j) + 20, svgEle.group, ENGINE_SCALE);
+          context.insert(engine);
+        }
+      }
+    
+      Config.sayHello();
+      ViewStack.reset(zoomgContainer.clientWidth, zoomgContainer.clientHeight, context);
+    
+      window.context = context;
+    }).catch(err => {
+      console.log(`ERROR occured during the process of initializing Zoomg svg-element.  The erros is:\n${err}`);
+    })
+
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="zoomg-container" style={{width: 1000, height: 600, backgroundColor: 'lightblue', overflow: 'scroll'}}>
+
     </div>
   );
 }
---
+++ src/shapes/atom.js
@@ -0,0 +1,120 @@
+const Zoomg = window.Zoomg;
+const Shape = Zoomg.Shape;
+const SVGComplexity = Zoomg.SVGComplexity;
+
+const svgStrings = {};
+svgStrings[SVGComplexity.Pixels120] = `
+  <g transform="translate(50, 54)" stroke-width="2">
+    <g fill="none">
+      <ellipse stroke="#66899a" rx="6" ry="44"></ellipse>
+      <ellipse stroke="#66899a" rx="6" ry="44"  transform="rotate(90)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(10)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(20)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(30)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(40)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(50)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(60)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(70)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(80)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-10)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(-20)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-30)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(-40)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-50)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(-60)"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-70)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(-80)"></ellipse>
+      <circle  stroke="#4b541f" r="44"></circle>
+    </g>
+    <g fill="#66899a" stroke="white">
+      <circle fill="#80a3cf" r="13"></circle>
+      <circle cy="-44" r="9"></circle>
+      <circle cx="-40" cy="18" r="9"></circle>
+      <circle cx="40" cy="18" r="9"></circle>
+    </g>
+  </g>
+`;
+
+svgStrings[SVGComplexity.Pixels60] = `
+<g transform="translate(50, 54)" stroke-width="2">
+  <g fill="none">
+    <ellipse stroke="#66899a" rx="6" ry="44"></ellipse>
+    <ellipse stroke="#66899a" rx="6" ry="44"  transform="rotate(90)"></ellipse>
+    <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(30)"></ellipse>
+    <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(60)"></ellipse>
+    <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-30)"></ellipse>
+    <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(-60)"></ellipse>
+    <circle  stroke="#4b541f" r="44"></circle>
+  </g>
+  <g fill="#66899a" stroke="white">
+    <circle fill="#80a3cf" r="13"></circle>
+    <circle cy="-44" r="9"></circle>
+    <circle cx="-40" cy="18" r="9"></circle>
+    <circle cx="40" cy="18" r="9"></circle>
+  </g>
+</g>
+`;
+
+svgStrings[SVGComplexity.Pixels30] = `
+<g transform="translate(50, 54)" stroke-width="2">
+  <g fill="none">
+    <ellipse stroke="#66899a" rx="6" ry="44"></ellipse>
+    <ellipse stroke="#66899a" rx="6" ry="44"  transform="rotate(90)"></ellipse>
+    <circle  stroke="#4b541f" r="44"></circle>
+  </g>
+  <g fill="#66899a" stroke="white">
+    <circle fill="#80a3cf" r="13"></circle>
+    <circle cy="-44" r="9"></circle>
+    <circle cx="-40" cy="18" r="9"></circle>
+    <circle cx="40" cy="18" r="9"></circle>
+  </g>
+</g>
+`;
+
+svgStrings[SVGComplexity.Pixels15] = `
+  <g transform="translate(50, 54)" stroke-width="2">
+    <g fill="#66899a" stroke="white">
+      <circle fill="#80a3cf" r="25"></circle>
+      <circle cy="-44" r="9"></circle>
+      <circle cx="-40" cy="18" r="9"></circle>
+      <circle cx="40" cy="18" r="9"></circle>
+    </g>
+  </g>
+`;
+
+svgStrings[SVGComplexity.Pixels7] = `
+  <g transform="translate(50, 54)" stroke-width="2">
+    <g fill="#66899a" stroke="white">
+      <circle fill="#80a3cf" r="25"></circle>
+    </g>
+  </g>
+`;
+
+export default class Atom extends Shape {
+
+  getTypeName() {
+    return "Atom";
+  }
+
+  renderContents(svg, svgComplexity) {
+    svg.innerHTML = svgStrings[svgComplexity];
+  }
+}
+
+/*
+Atom.alertStr = `
+  <g transform="translate(50, 54)" stroke-width="2">
+    <g fill="#FF0000">
+      <ellipse stroke="#66899a" rx="6" ry="44"></ellipse>
+      <ellipse stroke="#e1d85d" rx="6" ry="44" transform="rotate(-66)"></ellipse>
+      <ellipse stroke="#80a3cf" rx="6" ry="44" transform="rotate(66)"></ellipse>
+      <circle  stroke="#4b541f" r="44"></circle>
+    </g>
+    <g fill="#FF0000" stroke="white">
+      <circle fill="#FF0000" r="13"></circle>
+      <circle cy="-44" r="9"></circle>
+      <circle cx="-40" cy="18" r="9"></circle>
+      <circle cx="40" cy="18" r="9"></circle>
+    </g>
+  </g>`
+*/
