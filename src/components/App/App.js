44b6eab8b3806c0e2bca134ac27efb571e49536b
Himansu Desai <himansudesai@gmail.com>
2021-01-14 13:52:52 -0800

    This is a truly hysterical, momentous occasion. Zoomg is now able to handle multiple instances on the same page.  So we've nominally adjusted the train app to have two different zoomg wrapper components on the same page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import ZoomgView from '../zoomg_view/ZoomgView';
+import SecondZoomg from '../second_zoomg/SecondZoomg';
 
 function App() {
-
   return (
     <React.Fragment>
       <h1>Hello</h1>
       <ZoomgView></ZoomgView>
+      <SecondZoomg></SecondZoomg>
       <h1>Zoomg</h1>
     </React.Fragment>
   );
--- src/components/second_zoomg/SecondZoomg.js
+++ src/components/second_zoomg/SecondZoomg.js
@@ -2,55 +2,55 @@ import {useEffect} from 'react';
 import Atom from '../../shapes/atom.js';
 import Engine from '../../shapes/engine.js';
 
-function ZoomgView() {
+function SecondZoomg() {
 
   useEffect(() => {
     const Zoomg = window['Zoomg'];
     const Config = Zoomg.Config;
     const Rectangle = Zoomg.Rectangle;
     
-    const zoomgContainer = document.getElementById("zoomg-container");
+    const zoomgContainer = document.getElementById("second-zoomg");
 
-    Zoomg.new('zoomg-container').then( (view) => {
+    Zoomg.new('second-zoomg').then( (view) => {
       const context = new Rectangle("top-context", 0, 0, null, 0, 0);
       const svgElement = view.getSVGElement();
-
-      const ROW_COUNT = 2;
+    
+      const ROW_COUNT = 5;
       const COLUMN_COUNT = 12;
     
-      const ATOM_SIZE = 50;
-      const ENGINE_SIZE = 50;
+      const ATOM_SIZE = 8;
+      const ENGINE_SIZE = 8;
       const anAtom = new Atom("dummy", 0, 0, svgElement);
       const anEngine = new Engine("dummy", 0, 0, svgElement);
       const ATOM_SCALE = Config.registerInitialSizeForShape(anAtom.getTypeName(), ATOM_SIZE);
       const ENGINE_SCALE = Config.registerInitialSizeForShape(anEngine.getTypeName(), ENGINE_SIZE);
     
-      let count = 0;
+      let count = 99000;
       for (let i = 0; i < COLUMN_COUNT; i++) {
         for (let j = 0; j < ROW_COUNT; j++) {
           let atom = new Atom(`${++count}`, (ATOM_SIZE * i), (ATOM_SIZE * j), svgElement, ATOM_SCALE);
           context.insert(atom);
         }
       }
-
-      // for (let i = 0; i < COLUMN_COUNT; i++) {
-      //   for (let j = 25; j < ROW_COUNT * 2; j++) {
-      //     let engine = new Engine(`${++count}`, (ENGINE_SIZE * i), (ENGINE_SIZE * j) + 20, svgElement, ENGINE_SCALE);
-      //     context.insert(engine);
-      //   }
-      // }
+    
+      for (let i = 0; i < COLUMN_COUNT; i++) {
+        for (let j = 25; j < ROW_COUNT * 2; j++) {
+          let engine = new Engine(`${++count}`, (ENGINE_SIZE * i), (ENGINE_SIZE * j) + 20, svgElement, ENGINE_SCALE);
+          context.insert(engine);
+        }
+      }
     
       Config.sayHello();
       view.initialize(context, zoomgContainer.clientWidth, zoomgContainer.clientHeight);
     }).catch(err => {
-      console.log(`ERROR occured during the process of initializing Zoomg View.  The erros is:\n${err}`);
+      console.log(`ERROR occured during the process of initializing Zoomg svg-element.  The erros is:\n${err}`);
     })
   });
 
   return (
-    <div id="zoomg-container" style={{width: 1000, height: 450, backgroundColor: 'lightblue', overflow: 'scroll'}}>
+    <div id="second-zoomg" style={{width: 1000, height: 50, backgroundColor: 'pink'}}>
     </div>
   );
 }
 
-export default ZoomgView;
+export default SecondZoomg;
