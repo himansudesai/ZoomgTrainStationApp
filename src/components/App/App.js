2d1ee5edab45b6c042fc1080e26baa952b9654dd
Himansu Desai <himansudesai@gmail.com>
2021-04-02 15:57:06 -0700

    Rework ZoomgView and Zoom2View to work off data/metadata and move the actual data/metadata declarations to App.  Now we have App passing down the exact same data set to two different views.  Also work in code to take advantage of Zoomg 'move event emit' and 'move shape command' to have two views that mirror each other.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,74 @@
 import React from 'react';
 import './App.css';
+import Atom from '../../shapes/atom';
+import Rectangles from '../../shapes/rectangles';
 import ZoomgView from '../zoomg_view/ZoomgView';
 import ZoomgView2 from '../zoomg_view_2/ZoomgView2';
-import SmallZoomgView from '../small_zoomg_view/SmallZoomgView';
-import SecondZoomg from '../second_zoomg/SecondZoomg';
 
 function App() {
+  const metadata = {
+    Atom: {
+      type: Atom,
+      percentSize: 8
+    },
+    Rectangles: {
+      type: Rectangles,
+      percentSize: 8
+    }
+  };
+  const data = [];
+  data.push({
+    id: "Atom1",
+    x: 15,
+    y: 15,
+    typeName: "Atom"
+  });
+  data.push({
+    id: "Atom2",
+    x: 45,
+    y: 45,
+    typeName: "Atom"
+  });
+  data.push({
+    id: "Rectangle1",
+    x: 65,
+    y: 45,
+    typeName: "Rectangles"
+  });
+
+  let zoomgRef = React.createRef();
+  let zoomg2Ref = React.createRef();
+
+
+  function zoomgEvent(event) {
+    console.log(`**********1 incoming event ${JSON.stringify(event)} ****************`);
+    if (event.name === 'SHAPE_MOVE') {
+      zoomg2Ref.current && zoomg2Ref.current.moveShape(event.details.id, event.details.x, event.details.y);
+    } else {
+      console.log(`\n\n ****** EVENT WAS NOT "SHAPE_MOVE" ???`);
+    }
+  }
+
+  function zoomg2Event(event) {
+    console.log(`**********2 incoming event ${JSON.stringify(event)} ****************`);
+    if (event.name === 'SHAPE_MOVE') {
+      zoomgRef.current && zoomgRef.current.moveShape(event.details.id, event.details.x, event.details.y);
+    } else {
+      console.log(`\n\n ****** EVENT WAS NOT "SHAPE_MOVE" ???`);
+    }
+  }
+
+
   return (
     <React.Fragment>
       <h1>Hello</h1>
       <div style={{marginLeft: '20px'}}>
         &nbsp;
-        <ZoomgView></ZoomgView>
+        <ZoomgView ref={zoomgRef} metadata={metadata} data={data} onEvent={zoomgEvent}></ZoomgView>
       </div>
       <div style={{marginLeft: '20px'}}>
         &nbsp;
-        <ZoomgView2></ZoomgView2>
+        <ZoomgView2 ref={zoomg2Ref} metadata={metadata} data={data} onEvent={zoomg2Event}></ZoomgView2>
       </div>
       <h1>Zoomg</h1>
     </React.Fragment>
@@ -24,5 +77,3 @@ function App() {
 
 export default App;
 
-{/* <SmallZoomgView></SmallZoomgView>
-<SecondZoomg></SecondZoomg> */}
--- src/components/zoomg_view_2/ZoomgView2.js
+++ src/components/zoomg_view_2/ZoomgView2.js
@@ -1,66 +1,43 @@
-import {useEffect} from 'react';
-import Atom from '../../shapes/atom.js';
-import Engine from '../../shapes/engine.js';
-import Rectangles from '../../shapes/rectangles';
-import House from '../../shapes/house';
+import React, {forwardRef, useEffect, useImperativeHandle} from 'react';
 import './ZoomgView2.css';
 
-function ZoomgView2() {
+
+const ZoomgView2 = forwardRef((props, ref) => {
+
+  let thisView;
+  console.log(`~~~~2 executing useImperativeHandle`);
+  useImperativeHandle(ref, (shapeId, percentX, percentY) => ({
+    moveShape(shapeId, percentX, percentY) {
+      console.log(`~~~~2 moveShape ${shapeId} ${percentX}/${percentY}`);
+      thisView.moveShape(shapeId, percentX, percentY);
+    }
+  }))
 
   useEffect(() => {
     const Zoomg = window['Zoomg'];
     const Rectangle = Zoomg.Rectangle;
     
     const zoomgContainer = document.getElementById("zoomg-container-2");
-    console.log(`++++ BEFORE Zoomg.new - client width = ${zoomgContainer.clientWidth}`);
 
-    Zoomg.createView(zoomgContainer).then( (view) => {
-      const context = new Rectangle("top-context", 0, 0, view);
+    const onEvent = function(event) {
+      console.log(`~~~~2 onEvent: ${JSON.stringify(event)}`)
+      props.onEvent(event);
+    }
 
-      const ATOM_SIZE_PERCENT = 2;
-      const ENGINE_SIZE_PERCENT = 0.1;
-      const RECTANGLES_SIZE_PERCENT = 0.1;
-      const HOUSE_SIZE_PERCENT = 0.6;
-      const anAtom = new Atom("dummy", 0, 0, view);
-      const anEngine = new Engine("dummy", 0, 0, view);
-      const aRectangles = new Rectangles("dummy", 0, 0, view);
-      const aHouse = new House("dummy", 0, 0, view);
-      const ATOM_SCALE = view.getConfig().registerInitialPercentSizeForShape(anAtom.getTypeName(), ATOM_SIZE_PERCENT);
-      const ENGINE_SCALE = view.getConfig().registerInitialPercentSizeForShape(anEngine.getTypeName(), ENGINE_SIZE_PERCENT);
-      const RECTANGLES_SCALE = view.getConfig().registerInitialPercentSizeForShape(aRectangles.getTypeName(), RECTANGLES_SIZE_PERCENT);
-      const HOUSE_SCALE = view.getConfig().registerInitialPercentSizeForShape(aHouse.getTypeName(), HOUSE_SIZE_PERCENT);
-    
-      const slots = new Array(2000);
-      let filledSlots = 0;
-      for (let i=0; i<1; i++) {
-        const slot = Math.floor(Math.random() * 4000);
-        if (!slots[slot]) {
-          ++filledSlots;
-          const column = (slot % 80);
-          const row = Math.floor(slot / 80);
-          // let atom = new Atom(`${Math.random() * 100000}`, column + 3, row * 3, view, ATOM_SCALE);
-          let atom = new Atom(`Atom${i}`, 50, 60, view, ATOM_SCALE);
-          slots[slot] = atom;
-          context.insert(atom);
-        }
+    Zoomg.createView(zoomgContainer, onEvent).then( (view) => {
+      thisView = view;
+      console.log(`~~~~~~2 creating view - ${thisView}`);
+      const context = new Rectangle("top-top", 0, 0, view);
+      const scalesByType = {};
+      for (const [typeName, typeMetadata] of Object.entries(props.metadata)) {
+        scalesByType[typeName] = view.getConfig().registerInitialPercentSizeForShape(typeName, typeMetadata.percentSize);
       }
 
-      let rectangles = new Rectangles(`${Math.random() * 100000}`, 10, 10, view, RECTANGLES_SCALE);
-      context.insert(rectangles);
+      props.data.forEach(data => {
+        let shape = new props.metadata[data.typeName].type(data.id, data.x, data.y, view, scalesByType[data.typeName]);
+        context.insert(shape);
+      })
 
-      let house = new House(`${Math.random() * 100000}`, 20, 20, view, HOUSE_SCALE);
-      context.insert(house);
-
-      console.log(`++++ FILLED SLOTS = ${filledSlots}`);
-
-      // for (let i=0; i<100; i=i+1) {
-      //   for (let j=0; j<100; j=j+1) {
-      //     let engine = new Engine(`${Math.random() * 100000}`, i * ENGINE_SIZE_PERCENT * 6, 6 * j * ENGINE_SIZE_PERCENT, view, ENGINE_SCALE);
-      //     context.insert(engine);
-      //   }
-      // }
-    
-      view.getConfig().sayHello();
       view.initialize(context, zoomgContainer.offsetLeft, zoomgContainer.offsetTop, zoomgContainer.clientWidth, zoomgContainer.clientHeight);
     }).catch(err => {
       console.log(`ERROR occured during the process of initializing Zoomg View.  The error is:\n${err}`);
@@ -71,6 +48,6 @@ function ZoomgView2() {
     <div id="zoomg-container-2" style={{width: 300, height: 200, overflow: 'scroll'}}>
     </div>
   );
-}
+})
 
 export default ZoomgView2;
