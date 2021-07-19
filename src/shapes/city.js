12dec45fae7e26474cdb49989caf34ae66312ca0
Himansu Desai <himansudesai@gmail.com>
2021-07-18 22:41:20 -0700

    Add 'city' shape and start to special code the layout when inside a city

---
+++ src/shapes/city.js
@@ -0,0 +1,40 @@
+const Zoomg = window.Zoomg;
+const Shape = Zoomg.Shape;
+const SVGComplexity = Zoomg.SVGComplexity;
+
+const svgStrings = {}
+svgStrings[SVGComplexity.Pixels120] = [
+  `<g transform="translate(0, 0) scale(0.1, 0.11)"`,
+  `><path d="M616 192H480V24c0-13.26-10.74-24-24-24H312c-13.26 0-24 10.74-24 24v72h-64V16c0-8.84-7.16-16-16-16h-16c-8.84 0-16 7.16-16 16v80h-64V16c0-8.84-7.16-16-16-16H80c-8.84 0-16 7.16-16 16v80H24c-13.26 0-24 10.74-24 24v360c0 17.67 14.33 32 32 32h576c17.67 0 32-14.33 32-32V216c0-13.26-10.75-24-24-24zM128 404c0 6.63-5.37 12-12 12H76c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12H76c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12H76c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm128 192c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm160 96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12V76c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm160 288c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40zm0-96c0 6.63-5.37 12-12 12h-40c-6.63 0-12-5.37-12-12v-40c0-6.63 5.37-12 12-12h40c6.63 0 12 5.37 12 12v40z">
+  </path>
+  </g>`];
+
+svgStrings[SVGComplexity.Pixels60] = svgStrings[SVGComplexity.Pixels120];
+
+svgStrings[SVGComplexity.Pixels30] = svgStrings[SVGComplexity.Pixels120];
+
+svgStrings[SVGComplexity.BarelyVisible] = svgStrings[SVGComplexity.Pixels120];
+
+svgStrings[SVGComplexity.Pixels90] = svgStrings[SVGComplexity.Pixels120];
+
+export default class City extends Shape {
+  setColors(colors) {
+    this.red = colors[0];
+    this.green = colors[1];
+    this.blue = colors[2];
+  }
+
+  getTypeName() {
+    return "City";
+  }
+
+  renderContents(svg, svgComplexity) {
+    const strs = svgStrings[svgComplexity];
+    svg.innerHTML = `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+  }
+
+  getRenderString(detailLevel) {
+    const strs = svgStrings[detailLevel];
+    return `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+ }
+}
