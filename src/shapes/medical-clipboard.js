32f5bdf9fca66e53fe82832aed16ac2de2529f91
Himansu Desai <himansudesai@gmail.com>
2021-07-21 12:08:20 -0700

    add 'medical-circle', 'medical-clipboard', 'medical-pump', 'prescription-bottle', 'prescription', 'radiation' and 'thermometer' shapes

---
+++ src/shapes/medical-clipboard.js
@@ -0,0 +1,38 @@
+const Zoomg = window.Zoomg;
+const Shape = Zoomg.Shape;
+const SVGComplexity = Zoomg.SVGComplexity;
+
+const svgStrings = {};
+svgStrings[SVGComplexity.Pixels120] = [
+  `<g transform="translate(0, 0) scale(0.19, 0.19)"`,
+  `><path d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm96 304c0 4.4-3.6 8-8 8h-56v56c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8v-56h-56c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h56v-56c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v56h56c4.4 0 8 3.6 8 8v48zm0-192c0 4.4-3.6 8-8 8H104c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h176c4.4 0 8 3.6 8 8v16z"/>
+  </g>`];
+
+svgStrings[SVGComplexity.Pixels60] = svgStrings[SVGComplexity.Pixels120];
+svgStrings[SVGComplexity.Pixels30] = svgStrings[SVGComplexity.Pixels120];
+svgStrings[SVGComplexity.BarelyVisible] = svgStrings[SVGComplexity.Pixels120];
+svgStrings[SVGComplexity.Pixels90] = svgStrings[SVGComplexity.Pixels120];
+svgStrings[SVGComplexity.Pixels150] = svgStrings[SVGComplexity.Pixels120];
+
+export default class MedicalClipboard extends Shape {
+  setColors(colors) {
+    this.red = colors[0];
+    this.green = colors[1];
+    this.blue = colors[2];
+  }
+
+  getTypeName() {
+    return "MedicalClipboard";
+  }
+
+  renderContents(svg, svgComplexity) {
+    const strs = svgStrings[svgComplexity];
+    svg.innerHTML = `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+  }
+
+  getRenderString(detailLevel) {
+    const strs = svgStrings[detailLevel];
+    return `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+ }
+}
+
---
+++ src/shapes/prescription-bottle.js
@@ -0,0 +1,39 @@
+const Zoomg = window.Zoomg;
+const Shape = Zoomg.Shape;
+const SVGComplexity = Zoomg.SVGComplexity;
+
+const svgStrings = {};
+svgStrings[SVGComplexity.Pixels120] = [
+    `<g transform="translate(0, 0) scale(0.19, 0.19)"`,
+    `><path d="M360 0H24C10.8 0 0 10.8 0 24v48c0 13.2 10.8 24 24 24h336c13.2 0 24-10.8 24-24V24c0-13.2-10.8-24-24-24zM32 480c0 17.6 14.4 32 32 32h256c17.6 0 32-14.4 32-32V128H32v352zm64-184c0-4.4 3.6-8 8-8h56v-56c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v56h56c4.4 0 8 3.6 8 8v48c0 4.4-3.6 8-8 8h-56v56c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8v-56h-56c-4.4 0-8-3.6-8-8v-48z"/>
+    </g>`];
+
+  svgStrings[SVGComplexity.Pixels60] = svgStrings[SVGComplexity.Pixels120];
+  svgStrings[SVGComplexity.Pixels30] = svgStrings[SVGComplexity.Pixels120];
+  svgStrings[SVGComplexity.BarelyVisible] = svgStrings[SVGComplexity.Pixels120];
+  svgStrings[SVGComplexity.Pixels90] = svgStrings[SVGComplexity.Pixels120];
+  svgStrings[SVGComplexity.Pixels150] = svgStrings[SVGComplexity.Pixels120];
+
+export default class PrescriptionBottle extends Shape {
+  setColors(colors) {
+    this.red = colors[0];
+    this.green = colors[1];
+    this.blue = colors[2];
+  }
+
+  getTypeName() {
+    return "PrescriptionBottle";
+  }
+
+  renderContents(svg, svgComplexity) {
+    const strs = svgStrings[svgComplexity];
+    svg.innerHTML = `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+  }
+
+  getRenderString(detailLevel) {
+    const strs = svgStrings[detailLevel];
+    return `${strs[0]} fill="rgb(${this.red},${this.green},${this.blue})" ${strs[1]}`;
+ }
+}
+
+
